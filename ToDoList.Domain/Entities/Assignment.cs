2f34fe109d55e759c69014a11ad9b6e717a73ded
Egor Doroshkevich <egor.doroshkevich@saritasa.com>
2023-03-24 12:28:11 +0500

    Add assignment domain model TDL-1

--- ToDoList.Domain/Entities/Assignment.cs
+++ ToDoList.Domain/Entities/Assignment.cs
@@ -16,18 +16,20 @@ public class Assignment
 	/// <summary>
 	/// Assignment's title.
 	/// </summary>
+	[MaxLength(50)]
 	[Required]
 	required public string Title { get; set; }
 
 	/// <summary>
 	/// Assignment's description.
 	/// </summary>
+	[MaxLength(500)]
 	public string? Description { get; set; }
 
 	/// <summary>
 	/// The date until assignment must be completed.
 	/// </summary>
-	public DateTime CompleteUntil { get; set; }
+	public DateTime? CompleteUntil { get; set; }
 
 	/// <summary>
 	/// The date when assignment has been completed.
